321b2dafb878b72b4c7f5640e1f0ec45480aeefa
Andre <andreferreira@live.co.uk>
2022-11-28 23:08:59 +0000

    getters and setters

--- OOP/index.php
+++ OOP/index.php
@@ -16,19 +16,38 @@ class User
     {
         return "$this->username added a new credential";
     }
+
+    // getters
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    // setters
+    public function setEmail($email)
+    {
+        if (strpos($email, '@') > -1) {
+            $this->email = $email;
+        }
+    }
 }
 
 $userOne = new User('James', 'james@email.com');
 $userTwo = new User('Steve', 'steve@email.com');
 
-echo 'the class belongs to ' . get_class($userOne) . '<br>';
-echo $userOne->username . '<br>';
-echo $userOne->email . '<br>';
-echo $userOne->addCredential() . '<br>';
+// echo 'the class belongs to ' . get_class($userOne) . '<br>';
+// echo $userOne->username . '<br>';
+// echo $userOne->email . '<br>';
+// echo $userOne->addCredential() . '<br>';
 
-echo $userTwo->username . '<br>';
-echo $userTwo->email . '<br>';
-echo $userTwo->addCredential() . '<br>';
+// echo $userTwo->username . '<br>';
+// echo $userTwo->email . '<br>';
+// echo $userTwo->addCredential() . '<br>';
+
+// echo $userOne->getEmail() . '<br>';
+// echo $userTwo->getEmail() . '<br>';
+
+echo $userOne->setEmail('userone@test.com');
 
 print_r(get_class_vars('User'));
 print_r(get_class_methods('User'));
