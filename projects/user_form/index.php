6d1871f06fe39386568bdeb35b6e320c1a29810f
Andre <andreferreira@live.co.uk>
2022-12-11 00:01:42 +0000

    new form

---
+++ projects/user_form/index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User form</title>
+</head>
+
+<body>
+
+    <div class="new-user">
+        <h2>Create new user</h2>
+        <form action="" method="POST">
+            <label for="username">Username:</label>
+            <input type="text" name="username">
+
+            <label for="email">Email:</label>
+            <input type="text" name="email">
+
+            <input type="submit" value="Submit" name="submit">
+        </form>
+    </div>
+</body>
+
+</html>
