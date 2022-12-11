6e231391eaf5d8d8679d013d2223d53cba086805
Andre <andreferreira@live.co.uk>
2022-12-11 00:45:31 +0000

    structure and styles added

--- projects/user_form/index.php
+++ projects/user_form/index.php
@@ -1,9 +1,9 @@
 <?php
-
-
+if (isset($_POST['submit'])) {
+    echo 'form submitted';
+}
+echo '';
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>User form</title>
 </head>
 
@@ -19,12 +20,12 @@
 
     <div class="new-user">
         <h2>Create new user</h2>
-        <form action="" method="POST">
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
             <label for="username">Username:</label>
-            <input type="text" name="username">
+            <input type="text" name="username" autocomplete="off">
 
             <label for="email">Email:</label>
-            <input type="text" name="email">
+            <input type="email" name="email" autocomplete="off">
 
             <input type="submit" value="Submit" name="submit">
         </form>
